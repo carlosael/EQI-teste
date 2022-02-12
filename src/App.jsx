f4332d6b81d8e27efefeb4993cbe404ac27a84f9
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-02-12 11:56:35 -0300

    Card principal criado.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main-container">
+        <div className="header">
+          <h1>Simulador de Investimentos</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
