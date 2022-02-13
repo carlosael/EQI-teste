26d20fd9790f6a332b85bfae8cf4bf1c16cedc63
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-02-13 10:56:46 -0300

    Botões de rendimento adicionados.

--- src/components/Cards/IndexingFees/index.jsx
+++ src/components/Cards/IndexingFees/index.jsx
@@ -1,8 +1,17 @@
+import informationIcon from "../../../assets/informationIcon.svg";
+import "./style.css";
+import Button from "../Button";
+
 function IndexingFees() {
   return (
     <div className="card">
       <div className="card-title">
         <p>Taxas de indexação</p>
+        <img src={informationIcon} alt="Ícone de informação" />
+      </div>
+      <div className="buttons">
+        <Button></Button>
+        <Button></Button>
       </div>
     </div>
   );
--- src/components/Cards/Yield/index.jsx
+++ src/components/Cards/Yield/index.jsx
@@ -1,8 +1,47 @@
+import informationIcon from "../../../assets/informationIcon.svg";
+import "./style.css";
+import Button from "../Button";
+import check from "../../../assets/check.svg";
+import { useState } from "react";
+
 function Yield() {
+  const [grossIncomeActive, setGrossIncomeActive] = useState(true);
+  const [netIncomeActive, setNetIncomeActive] = useState(false);
+
+  function handleIncomeType() {
+    if (grossIncomeActive) {
+      setGrossIncomeActive(false);
+      setNetIncomeActive(true);
+    } else {
+      setGrossIncomeActive(true);
+      setNetIncomeActive(false);
+    }
+  }
+
   return (
     <div className="card">
       <div className="card-title">
         <p>Rendimentos</p>
+        <img src={informationIcon} alt="Ícone de informação" />
+      </div>
+      <div className="buttons-container">
+        <Button
+          color={grossIncomeActive ? "active" : ""}
+          classes={"wider"}
+          action={handleIncomeType}
+        >
+          {grossIncomeActive && <img src={check} alt="check icon" />}
+          Bruto
+        </Button>
+
+        <Button
+          color={netIncomeActive ? "active" : ""}
+          classes={"wider"}
+          action={handleIncomeType}
+        >
+          {netIncomeActive && <img src={check} alt="check icon" />}
+          Líquido
+        </Button>
       </div>
     </div>
   );
