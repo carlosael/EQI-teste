12b5dce2f35a00d61d0cf559c002c822d95f6eea
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-02-13 17:13:17 -0300

    Botão Simular configurado.

--- src/components/MainButtons/index.jsx
+++ src/components/MainButtons/index.jsx
@@ -1,9 +1,27 @@
 import Button from "../Button";
 import ValuesContext from "../../contexts/ValuesContext";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 
 export default function MainButtons() {
+  const [buttonActive, setButtonActive] = useState(false);
   const data = useContext(ValuesContext);
+  console.log(data.form.profitability);
+
+  useEffect(() => {
+    if (
+      data.form.aport !== "" &&
+      data.form.monthlyAport !== "" &&
+      data.form.profitability !== "" &&
+      data.form.term !== ""
+    ) {
+      setButtonActive(true);
+    }
+  }, [
+    data.form.aport,
+    data.form.monthlyAport,
+    data.form.profitability,
+    data.form.term,
+  ]);
 
   function handleClearInputs() {
     data.setForm(data.defaultFormValues);
@@ -13,7 +31,10 @@ export default function MainButtons() {
       <Button classes={"wider"} action={handleClearInputs}>
         <strong>Limpar campos</strong>
       </Button>
-      <Button classes={"wider"}>
+      <Button
+        classes={"wider"}
+        color={buttonActive ? "activated" : "desactive"}
+      >
         <strong>Simular</strong>
       </Button>
     </>
